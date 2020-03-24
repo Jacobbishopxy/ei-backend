8e84e34dd6ccddb753fbcbc01f4241e166220e25
Jacob Xie <jacobbishopxy@gmail.com>
2020-03-24 13:36:49 +0800

    MNT: update DevMongo.scala -- create collection with `validationOptions`

--- src/test/scala/DevMongo.scala
+++ src/test/scala/DevMongo.scala
@@ -1,13 +1,17 @@
 
+import com.mongodb.client.model.{Filters, ValidationOptions}
 import com.typesafe.config.ConfigFactory
+import org.bson.BsonType
 import org.mongodb.scala._
 import org.mongodb.scala.bson.codecs.Macros._
 import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
 import org.mongodb.scala.bson.annotations.BsonProperty
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
 import org.bson.codecs.configuration.CodecRegistry
+import org.bson.conversions.Bson
+import org.mongodb.scala.model.CreateCollectionOptions
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
 /**
@@ -21,7 +25,7 @@ object DevMongo extends App {
   val mongoUrl = config.getString("mongo.url")
 
   val mongoClient = MongoClient(mongoUrl)
-  val database = mongoClient.getDatabase("dev").withCodecRegistry(codecRegistry)
+  val database: MongoDatabase = mongoClient.getDatabase("dev").withCodecRegistry(codecRegistry)
   val collection: MongoCollection[InfoDB] = database.getCollection("test")
 
 
@@ -48,5 +52,32 @@ object Repo {
 
   val codecRegistry: CodecRegistry =
     fromRegistries(fromProviders(classOf[Cord], classOf[InfoDB]), DEFAULT_CODEC_REGISTRY)
+
+
+  // mongo create collection with validation options
+  /*
+  {
+      "validator": { "$and":
+         [
+            { "username": { "$type": "string" } },
+            { "email": { "$regex": "@*.*$" } },
+            { "password": { "$type": "string" } }
+         ]
+      }
+   }
+   */
+
+  val username: Bson = Filters.`type`("username", BsonType.STRING)
+  val email: Bson = Filters.regex("email", "@*.*$")
+  val password: Bson = Filters.`type`("password", BsonType.STRING)
+
+  val validator: Bson = Filters.and(username, email, password)
+
+  val validationOptions: ValidationOptions = new ValidationOptions().validator(validator)
+
+  def createUserCollection(colName: String, db: MongoDatabase): Future[Completed] = {
+    val co = new CreateCollectionOptions().validationOptions(validationOptions)
+    db.createCollection(colName, co).toFuture()
+  }
 }
 
