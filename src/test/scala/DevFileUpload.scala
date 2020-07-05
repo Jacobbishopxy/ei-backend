51d4944a9057c8d30e3ac73d361c2cb59392d7a1
Jacob <jacobbishopxy@gmail.com>
2020-07-05 15:24:32 +0800

    MNT: update test/DevFileUpload.scala -- add `fileDownload` routing

--- src/test/scala/DevFileUpload.scala
+++ src/test/scala/DevFileUpload.scala
@@ -1,25 +1,30 @@
 import akka.actor.ActorSystem
-import akka.http.javadsl.server.directives.FileInfo
+import akka.http.scaladsl.server.directives.FileInfo
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model._
 import akka.stream.Materializer
 import akka.stream.scaladsl.Framing
 import akka.util.ByteString
+
 import java.io.File
 
-import akka.http.scaladsl.model.StatusCodes
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.io.StdIn
 
 /**
- * Created by Jacob Xie on 4/23/2020
+ * Created by Jacob Xie on 7/5/2020
  */
 object DevFileUpload {
 
+  def fd: Route = path("fileDownload") {
+    complete(HttpEntity.fromFile(ContentTypes.`application/octet-stream`, new File("./README.md")))
+  }
+
   def fu(implicit m: Materializer, e: ExecutionContextExecutor): Route = path("fileUpload") {
-    fileUpload("csv1") {
+    fileUpload("csv") {
       case (metadata, byteSource) =>
 
         println("byteSource: ", byteSource)
@@ -32,12 +37,12 @@ object DevFileUpload {
             .map(_.toInt)
             .runFold(0) { (acc, n) => acc + n }
 
-        onSuccess(sumF) { sum => complete(s"c1 Sum: $sum") }
+        onSuccess(sumF) { sum => complete(s"Sum: $sum") }
     }
   }
 
-  def fus(implicit m: Materializer, e: ExecutionContextExecutor): Route = path("file") {
-    fileUploadAll("csv2") {
+  def fus(implicit m: Materializer, e: ExecutionContextExecutor): Route = path("fileUploads") {
+    fileUploadAll("csv") {
       byteSources =>
 
         println("byteSources: ", byteSources)
@@ -55,7 +60,7 @@ object DevFileUpload {
             accF.flatMap(acc => intF.map(acc + _))
         }
 
-        onSuccess(sumF) { sum => complete(s"c2 Sum: $sum") }
+        onSuccess(sumF) { sum => complete(s"Sum: $sum") }
     }
   }
 
@@ -91,6 +96,7 @@ object DevFileUpload {
     implicit val ect: ExecutionContextExecutor = materializer.executionContext
 
     concat(
+      fd,
       fu,
       fus,
       suf,
