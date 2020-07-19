8d722182586f25aa11331b483daf72669926c1f8
Jacob <jacobbishopxy@gmail.com>
2020-07-19 21:43:05 +0800

    MNT: update eiDashboard/Model.scala -- add ProModel

--- src/main/scala/com/github/jacobbishopxy/eiDashboard/Model.scala
+++ src/main/scala/com/github/jacobbishopxy/eiDashboard/Model.scala
@@ -42,3 +42,43 @@ object Model {
   implicit val gridTPFormat: RootJsonFormat[GridTemplatePanel] = jsonFormat2(GridTemplatePanel)
 
 }
+
+object ProModel {
+
+  /*
+  anchor
+      |___store
+      |___element
+   */
+
+  case class Anchor(identity: String, category: String, symbol: String, date: String) // maybe more key?
+
+  case class Content(data: String, config: Option[String])
+  case class Store(anchor: Anchor, content: Content)
+
+  case class Coordinate(x: Int, y: Int, h: Int, w: Int)
+  case class Element(anchor: Anchor, coordinate: Coordinate)
+
+  case class GridLayout(template: String, panel: String, layouts: Seq[Element])
+  case class GridTemplatePanel(template: String, panel: String)
+
+
+  val CR: CodecRegistry = fromRegistries(fromProviders(
+    classOf[Anchor],
+    classOf[Content],
+    classOf[Store],
+    classOf[Coordinate],
+    classOf[Element],
+    classOf[GridLayout],
+    classOf[GridTemplatePanel],
+  ))
+
+  implicit val anchorFormat: RootJsonFormat[Anchor] = jsonFormat4(Anchor)
+  implicit val contentFormat: RootJsonFormat[Content] = jsonFormat2(Content)
+  implicit val storeFormat: RootJsonFormat[Store] = jsonFormat2(Store)
+  implicit val coordinateFormat: RootJsonFormat[Coordinate] = jsonFormat4(Coordinate)
+  implicit val elementFormat: RootJsonFormat[Element] = jsonFormat2(Element)
+  implicit val gridLayoutFormat: RootJsonFormat[GridLayout] = jsonFormat3(GridLayout)
+  implicit val gridTemplatePanelFormat: RootJsonFormat[GridTemplatePanel] = jsonFormat2(GridTemplatePanel)
+}
+
